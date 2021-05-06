849262491204b262aafa2fc997e637438a6521c2
Russell L Friesenhahn <russellf@utexas.edu>
2021-05-06 08:27:16 -0500

    Add helper scripts for environment

---
+++ scripts/source_env-stc0_caravel_user_project.sh
@@ -0,0 +1,6 @@
+export CARAVEL_ROOT=$(pwd)/caravel
+# specify simulation mode: RTL/GL
+#export SIM=RTL
+export OPENLANE_ROOT=$(pwd)/modules/openlane
+export OPENLANE_TAG=v0.15
+export PDK_ROOT=$(pwd)/modules/pdks
